cd9bd2e0d7871f2eba9da6ea9df558ce72596463
savaseso <rigbence@gmail.com>
2019-02-13 21:23:35 +0100

    Todos component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
-import './App.css';
+import Todos from './components/Todos';
 
 class App extends Component {
+  state = {
+    todos: [
+      { id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      { id: 2,
+        title: 'Dinner with Dori',
+        completed: false
+      },
+      { id: 3,
+        title: 'Meeting with boss',
+        completed: false
+      }
+    ]
+  }
   render() {
+    console.log(this.props.todos)
     return (
       <div className="App">
-        <h1>app</h1>
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
