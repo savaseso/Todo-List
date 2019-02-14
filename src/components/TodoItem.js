e4ad41226732ce43e20506d0eca7d3d4872516ea
savaseso <rigbence@gmail.com>
2019-02-14 21:25:46 +0100

    delete  button created

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -11,12 +11,16 @@ export class TodoItem extends Component {
       textDecoration:this.props.todo.completed ? 'line-through':'none'
     }
   }
+
   render() {
+    const {id, title} = this.props.todo;
     return (
         <div style={this.getStyle()}>
           <p>
-          <input type='checkbox' />{' '}
-          {this.props.todo.title}
+          <input type='checkbox' onChange={this.props.markComplete.bind(this, id)} />{' '}
+          { title }
+          <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>x</button>
+          
           </p>
         </div>
     )
@@ -28,4 +32,14 @@ TodoItem.propType = {
   todo: PropTypes.object.isRequired
 }
 
-export default TodoItem
+const btnStyle = {
+  background: '#ff0000',
+  color:'#fff',
+  border: 'none',
+  padding: '5px 9px',
+  borderRadius: '50%',
+  cursor: 'pointer',
+  float: 'right'
+}
+
+export default TodoItem;
