1532187a4e8434df6d02fcff5a80516066db3923
savaseso <rigbence@gmail.com>
2019-03-01 04:04:58 +0100

    header added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Todos from './components/Todos';
+import Header from './components/layout/Header'
 
 class App extends Component {
   state = {
@@ -37,6 +38,7 @@ this.setState({todos:[...this.state.todos.filter(todo=>todo.id!==id)]});
   render() {
     return (
       <div className="App">
+        <Header />
         <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
       </div>
     );
